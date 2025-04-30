920c0dc10da56d9c36fa8ebe8ebd41d637437674
dale-handley <144903128+dale-handley@users.noreply.github.com>
2025-04-30 08:46:01 +0100

    Update all_patid_ethnicity.R

--- cohort_scripts/all_patid_ethnicity.R
+++ cohort_scripts/all_patid_ethnicity.R
@@ -86,3 +86,71 @@ gp_qrisk2_ethnicity <- raw_gp_ethnicity %>%
   select(patid, gp_qrisk2_ethnicity=qrisk2_ethnicity_cat) %>%
   analysis$cached("gp_qrisk2_ethnicity",unique_indexes="patid", indexes="gp_qrisk2_ethnicity")
 
+# Find ethnicity from HES codes
+
+hes_ethnicity <- cprd$tables$hesPatient %>% 
+  filter(!is.na(gen_ethnicity)) %>% 
+  mutate(hes_5cat_ethnicity=case_when(gen_ethnicity==1 ~ 0L,
+                                      gen_ethnicity==2 ~ 2L,
+                                      gen_ethnicity==3 ~ 2L,
+                                      gen_ethnicity==4 ~ 2L,
+                                      gen_ethnicity==5 ~ 1L,
+                                      gen_ethnicity==6 ~ 1L,
+                                      gen_ethnicity==7 ~ 1L,
+                                      gen_ethnicity==8 ~ 1L, # Other Asian -> South Asian as per GP codes
+                                      gen_ethnicity==9 ~ 3L,
+                                      gen_ethnicity==10 ~ 4L,    
+                                      gen_ethnicity==11 ~ 3L),
+         hes_16cat_ethnicity=case_when(gen_ethnicity==1 ~ 1L,     # Unspecified white -> British as per GP codes
+                                       gen_ethnicity==2 ~ 12L,
+                                       gen_ethnicity==3 ~ 13L,
+                                       gen_ethnicity==4 ~ 14L,
+                                       gen_ethnicity==5 ~ 8L,
+                                       gen_ethnicity==6 ~ 9L,
+                                       gen_ethnicity==7 ~ 10L,
+                                       gen_ethnicity==8 ~ 11L,
+                                       gen_ethnicity==9 ~ 15L,
+                                       gen_ethnicity==10 ~ 7L,    # Unspecified mixed -> Other mixed as per GP records
+                                       gen_ethnicity==11 ~ 16L),
+         hes_qrisk2_ethnicity=case_when(gen_ethnicity==1 ~ 1L,     # Unspecified white -> British as per GP codes
+                                        gen_ethnicity==2 ~ 6L,
+                                        gen_ethnicity==3 ~ 7L,
+                                        gen_ethnicity==4 ~ 9L,
+                                        gen_ethnicity==5 ~ 2L,
+                                        gen_ethnicity==6 ~ 3L,
+                                        gen_ethnicity==7 ~ 4L,
+                                        gen_ethnicity==8 ~ 5L,
+                                        gen_ethnicity==9 ~ 8L,
+                                        gen_ethnicity==10 ~ 9L,    
+                                        gen_ethnicity==11 ~ 9L)) %>%
+  analysis$cached("hes_ethnicity",unique_indexes="patid", indexes=c("hes_5cat_ethnicity", "hes_16cat_ethnicity", "hes_qrisk2_ethnicity"))
+
+
+############################################################################################
+
+## Combine GP and HES ethnicity
+## Set 16- and QRISK-category ethnicity to missing if directly conflicts with 5-category ethnicity, before using HES ethnicity for missing
+ethnicity <- cprd$tables$patient %>%
+  select(patid) %>%
+  left_join(gp_5cat_ethnicity, by="patid") %>%
+  left_join(gp_16cat_ethnicity, by="patid") %>%
+  left_join(gp_qrisk2_ethnicity, by="patid") %>%
+  left_join((hes_ethnicity %>% select(-pracid)), by="patid") %>%
+  
+  mutate(gp_16cat_ethnicity=ifelse((gp_5cat_ethnicity==0 & (gp_16cat_ethnicity==8 | gp_16cat_ethnicity==9 | gp_16cat_ethnicity==10 | gp_16cat_ethnicity==11 | gp_16cat_ethnicity==12 | gp_16cat_ethnicity==13 | gp_16cat_ethnicity==14 | gp_16cat_ethnicity==15)) |
+                                     (gp_5cat_ethnicity==1 & (gp_16cat_ethnicity==1 | gp_16cat_ethnicity==2 | gp_16cat_ethnicity==3 | gp_16cat_ethnicity==4 | gp_16cat_ethnicity==5 | gp_16cat_ethnicity==12 | gp_16cat_ethnicity==13 | gp_16cat_ethnicity==14 | gp_16cat_ethnicity==15)) |
+                                     (gp_5cat_ethnicity==2 & (gp_16cat_ethnicity==1 | gp_16cat_ethnicity==2 | gp_16cat_ethnicity==3 | gp_16cat_ethnicity==6 | gp_16cat_ethnicity==8 | gp_16cat_ethnicity==9 | gp_16cat_ethnicity==10 | gp_16cat_ethnicity==11 | gp_16cat_ethnicity==15)), NA, gp_16cat_ethnicity),
+         
+         gp_qrisk2_ethnicity=ifelse((gp_5cat_ethnicity==0 & (gp_qrisk2_ethnicity==2 | gp_qrisk2_ethnicity==3 | gp_qrisk2_ethnicity==4 | gp_qrisk2_ethnicity==5 | gp_qrisk2_ethnicity==6 | gp_qrisk2_ethnicity==7 | gp_qrisk2_ethnicity==8)) |
+                                      (gp_5cat_ethnicity==1 & (gp_qrisk2_ethnicity==1 | gp_qrisk2_ethnicity==6 | gp_qrisk2_ethnicity==7 | gp_qrisk2_ethnicity==8)) |
+                                      (gp_5cat_ethnicity==2 & (gp_qrisk2_ethnicity==1 | gp_qrisk2_ethnicity==2 | gp_qrisk2_ethnicity==3 | gp_qrisk2_ethnicity==4 | gp_qrisk2_ethnicity==5 | gp_qrisk2_ethnicity==8)), NA, gp_qrisk2_ethnicity),
+         
+         ethnicity_5cat=coalesce(gp_5cat_ethnicity, hes_5cat_ethnicity),
+         ethnicity_16cat=ifelse(!is.na(hes_5cat_ethnicity) & hes_5cat_ethnicity==ethnicity_5cat, coalesce(gp_16cat_ethnicity, hes_16cat_ethnicity), gp_16cat_ethnicity),
+         ethnicity_qrisk2=ifelse(!is.na(hes_5cat_ethnicity) & hes_5cat_ethnicity==ethnicity_5cat, coalesce(gp_qrisk2_ethnicity, hes_qrisk2_ethnicity), gp_qrisk2_ethnicity)) %>%
+  
+  mutate(ethnicity_qrisk2=ifelse(is.na(ethnicity_qrisk2), 0L, ethnicity_qrisk2)) %>%
+  
+  select(patid, ethnicity_5cat, ethnicity_16cat, ethnicity_qrisk2) %>%
+  
+  analysis$cached("ethnicity", unique_indexes="patid")
