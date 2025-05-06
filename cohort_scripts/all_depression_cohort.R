3fe5f15259b602384eac44a18c4cc65790eb5cc7
dale-handley <144903128+dale-handley@users.noreply.github.com>
2025-05-06 20:58:08 +0100

    Update all_depression_cohort.R

--- cohort_scripts/all_depression_cohort.R
+++ cohort_scripts/all_depression_cohort.R
@@ -1,6 +1,15 @@
+#Script for creating the Depression cohort
+#Note: in this script, we exclude severe mental illness last,
+#As it is project-specific how these IDs are dealt with.
+#The generation of tables in this script can seem verbose, but it is designed
+#To be "plug and play" at any point in the QC, as required for different projects.
+
+
 library(aurum)
 library(tidyverse)
 
+
+####Connection and code lists####
 #Open connection
 #Connect to the MySQL database
 cprdenvname <- "CPRD_depression_data"
@@ -18,27 +27,30 @@ dep_qof_codes <- codes$depression_qof
 dep_all_codes <- codes$depression_broad
 dep_sympts_codes <- codes$add_sympts_dep
 
+#NOTRUN: updating the broad depression to contain all codes in the QOF depression. 
+
 #bring local
-dep_qof_codes_local <- dep_qof_codes %>% collect()
-dep_all_codes_local <- dep_all_codes %>% collect()
+#dep_qof_codes_local <- dep_qof_codes %>% collect()
+#dep_all_codes_local <- dep_all_codes %>% collect()
 
-#Check all QOF codes are in broad depression codes.
-nrow(dep_qof_codes_local) #195 codes
-nrow(dep_all_codes_local) #63 additional codes.
+#Add the missing code to the df.
+#dep_all <- dep_all_codes_local %>% 
+#  bind_rows(tibble(medcodeid = as.character(3071801000006112), depression_broad_cat = "diagnosis"))
 
-#Check the overlap between code lists.
-matching_codes <- dep_all_codes_local %>% filter(medcodeid %in% dep_qof_codes_local$medcodeid)
-nrow(matching_codes) #194
+#dep_all %>%  codesets$loadMedCodeSet(category = "depression_broad_cat", name = "depression_broad_ext", version = "03/05/2025")
 
-missing_code <- dep_qof_codes_local %>%
-  filter(!(medcodeid %in% dep_all_codes_local$medcodeid))
+#inner join the code lists and check length to make sure all codes are present
+#dep_check <- codesets$getAllCodeSetVersion("03/05/2025")
+#dep_all_codes_ext <- dep_check$depression_broad_ext
 
-print(missing_code)
+#Check for missing codes
+#missing_code <- dep_qof_codes %>% anti_join(dep_all_codes_ext) %>% count() #0 - all QOF codes are covered by the broad depression now.
 
+#Check they are all listed as the same type. 
+#code_match_length <- dep_all_codes_ext %>% inner_join(dep_qof_codes) %>% 
+#  filter(depression_broad_ext_cat == depression_qof_cat) %>% count()
 
-
-#1 code is missing - which one?
-
+####Baseline QC####
 #First check the quality of the data
 cprd$tables$patient %>% count() #3984014 - total patient count in download
 cprd$tables$patient %>% filter(acceptable ==1) %>% count() #3984014
@@ -67,13 +79,91 @@ gender_exclusion_ids <- cprd$tables$patient %>%
 gender_exclusion_ids %>% count()
 cprd$tables$patient %>% anti_join(practice_exclusion_ids, by="patid") %>% anti_join(gender_exclusion_ids, by="patid") %>% count() #3934754
 
-#Define the depression cohort
-#first, calculate a list of individuals who have at least 1 QOF depression code that is diagnostic.
-
+####Define the depression cohort####
 analysis <- cprd$analysis("dh")
 
+#Extract depression symptoms
 raw_depression_sympts_medcodes <- cprd$tables$observation %>%
   inner_join(dep_sympts_codes, by="medcodeid") %>%
   analysis$cached("raw_depression_sympts_medcodes", indexes=c("patid", "obsdate", "add_sympts_dep_cat"))
+
+#Broad depression extended
+raw_broad_depression_medcodes <- cprd$tables$observation %>%
+  inner_join(dep_all_codes_ext, by="medcodeid") %>%
+  analysis$cached("raw_broad_depression_ext_medcodes", indexes=c("patid", "obsdate", "depression_broad_ext_cat"))
+  
+#And QOF depression
+raw_qof_depression_medcodes <- cprd$tables$observation %>%
+  inner_join(dep_qof_codes, by="medcodeid") %>%
+  analysis$cached("raw_qof_depression_medcodes", indexes=c("patid", "obsdate", "dep_qof_cat"))
+
+#Re-load the broad and QOF codes
+analysis <- cprd$analysis("dh")
+raw_qof_depression_medcodes <- raw_qof_depression_medcodes %>% analysis$cached("raw_qof_depression_medcodes")
+raw_broad_depression_ext_medcodes <- raw_broad_depression_medcodes %>% analysis$cached("raw_broad_depression_ext_medcodes")
+
+####Ensure valid dates####
+analysis <- cprd$analysis("all_patid")
+
+#Load table which contains the least of GP, death, and deregistration dates: 
+valid_dates <- valid_dates %>% analysis$cached("death_end_dat")
+valid_dates %>% count()
+
+
+analysis <- cprd$analysis("dh")
+
+#Find individuals with a valid QOF code
+qof_ids <- raw_qof_depression_medcodes %>%
+  inner_join(valid_dates, by="patid") %>%
+  filter(obsdate>=min_dob & obsdate<=gp_death_end_date) %>%
+  distinct(patid) %>%
+  analysis$cached("qof_depression_medcodes", unique_indexes="patid")
+
+#And a valid broad depression id. 
+broad_ext_medcodes <- raw_broad_depression_ext_medcodes %>%
+  inner_join(valid_dates, by="patid") %>%
+  filter(obsdate>=min_dob & obsdate<=gp_death_end_date) %>%
+  analysis$cached("raw_broad_depression_medcodes_valid", unique_indexes="patid")
+
+broad_ext_ids <- raw_broad_depression_ext_medcodes %>%
+  inner_join(valid_dates, by="patid") %>%
+  distinct(patid) %>%
+  analysis$cached("raw_broad_depression_ext_ids_valid", unique_indexes="patid")
+
+
+####Recap 1####
+analysis <- cprd$analysis("dh")
+qof_ids <- qof_ids %>% analysis$cached("qof_depression_ids")
+
+analysis <- cprd$analysis("all_patid")
+practice_exclusion_ids <- practice_exclusion_ids %>% analysis$cached("practice_exclusion_ids")
+gender_exclusion_ids <- gender_exclusion_ids %>% analysis$cached("gender_exclusion_ids")
+
+#So far:
+practice_exclusion_ids %>% count() #48912 removed by practice
+
+cprd$tables$patient %>% anti_join(practice_exclusion_ids, by="patid") %>% 
+  anti_join(gender_exclusion_ids, by="patid") %>% count() #3934754 after removing gender exclusions
+
+cprd$tables$patient %>%  anti_join(practice_exclusion_ids, by="patid") %>% 
+  anti_join(gender_exclusion_ids, by="patid") %>% inner_join(qof_ids) %>% count() #3934754
+
+#Save the above table for later.
+depression_cohort_ids <- qof_ids %>%
+  anti_join(practice_exclusion_ids, by="patid") %>%
+  anti_join(gender_exclusion_ids, by="patid") %>%
+  analysis$cached("depression_cohort_ids_incsmi", unique_indexes="patid")
+
+
+#Reload the codes
+raw_qof_depression_medcodes <- raw_qof_depression_medcodes %>% analysis$cached("raw_qof_depression_medcodes")
+raw_broad_depression_medcodes_valid <- raw_broad_depression_medcodes_valid %>% analysis$cached("raw_broad_depression_medcodes_valid")
+
+#Now, we use the broad code list, and remove individuals where their first code is
+#not a diagnostic code.
+#obstype = 4 is filtered as these are family history codes.
+
+
+
 
   
