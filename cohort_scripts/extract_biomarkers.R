24ffc01ac9211a29774a8ce5ef4826612916b1f4
dale-handley <144903128+dale-handley@users.noreply.github.com>
2025-05-07 10:09:44 +0100

    Update extract_biomarkers.R

--- cohort_scripts/extract_biomarkers.R
+++ cohort_scripts/extract_biomarkers.R
@@ -1,1 +1,33 @@
+library(EHRBiomarkr)
+library(aurum)
+library(tidyverse)
+
+cprdenvname <- "CPRD_depression_data"
+yaml <- ""
+
+#Open the CPRD data
+cprd <- CPRDData$new(cprdEnv = cprdenvname, cprdConf = yaml)
+codesets = cprd$codesets()
+codes = codesets$getAllCodeSetVersion("31/10/2021")
+
+#List of biomarkers to extract
+biomarkers <- c("weight", "height", "bmi", "hdl", "triglyceride", "creatinine_blood", "ldl", "alt", "ast", "totalcholesterol", "dbp", "sbp", "acr")
+
+
+#Load cohort and extract raw data:
+analysis = cprd$analysis("all_patid")
+
+for (i in biomarkers) {
+  
+  print(i)
+  
+  raw_tablename <- paste0("raw_", i, "_medcodes")
+  
+  data <- cprd$tables$observation %>%
+    inner_join(codes[[i]], by="medcodeid") %>%
+    analysis$cached(raw_tablename, indexes=c("patid", "obsdate", "testvalue", "numunitid"))
+  
+  assign(raw_tablename, data)
+  
+}
 
