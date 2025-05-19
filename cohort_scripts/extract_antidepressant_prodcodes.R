ca0992277321dda7c7aacbf060987f3d90734e61
dale-handley <144903128+dale-handley@users.noreply.github.com>
2025-05-19 09:54:49 +0100

    Update extract_antidepressant_prodcodes.R

--- cohort_scripts/extract_antidepressant_prodcodes.R
+++ cohort_scripts/extract_antidepressant_prodcodes.R
@@ -18,6 +18,7 @@ analysis <- cprd$analysis("all_patid")
 
 drugs <- names(codes)
 
+#Extract raw codes
 for (i in drugs){
   
   if (length(codes[[i]]) > 0) {
@@ -34,3 +35,32 @@ for (i in drugs){
     
   }
 }
+
+analysis <- cprd$analysis("all_patid")
+valid_dates <- valid_dates %>% analysis$cached("death_end_dat")
+
+#raw_AD_SSRI_prodcodes %>% count() - 
+
+#Clean codes
+for(i in drugs){
+  
+  clean_tablename <- paste0("clean_", i, "_prodcodes")
+  raw_tablename <- paste0("raw_", i, "_prodcodes")
+  drug_class <- i
+  
+  #Names for dynamically saving table
+  current_cat_sym <- sym(current_cat)
+  index_cols <- c("patid", "date", "chem_name")
+  
+  print(paste0("Making ", drug_class, " clean prodcode table"))
+  
+  #Create clean table.
+  data <- get(raw_tablename) %>% inner_join(valid_dates, by="patid") %>%
+    filter(issuedate>=min_dob & issuedate<=gp_death_end_date) %>%
+    mutate(drug_class = drug_class) %>% 
+    select(patid, date=issuedate, dosageid, quantity, quantunitid, duration, chem_name = !!current_cat_sym, drug_class) %>%
+    analysis$cached(clean_tablename, indexes=index_cols)
+}
+
+
+
