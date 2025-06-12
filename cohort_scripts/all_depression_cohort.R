c6214dceba3ea91e2245ed437895f57690934061
dale-handley <144903128+dale-handley@users.noreply.github.com>
2025-06-12 12:30:23 +0100

    Update all_depression_cohort.R

--- cohort_scripts/all_depression_cohort.R
+++ cohort_scripts/all_depression_cohort.R
@@ -13,8 +13,8 @@ rm(list = ls())
 ####Connection and code lists####
 #Open connection
 #Connect to the MySQL database
-cprdenvname <- "CPRD_depression_data"
-yaml <- "C:/Users/dhand/.ssh/.aurum.yaml"
+cprdenvname <- ""
+yaml <- ""
 
 #open connection and get codes sets
 cprd = CPRDData$new(cprdEnv = cprdenvname, cprdConf = yaml)
