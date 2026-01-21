add3f32647924142a749462f29e92094db5f2a47
dale-handley <144903128+dale-handley@users.noreply.github.com>
2026-01-21 15:08:38 +0000

    Update create_hospitalisation_for_depression.R

--- cohort_scripts/depresssion_phenotypes/create_hospitalisation_for_depression.R
+++ cohort_scripts/depresssion_phenotypes/create_hospitalisation_for_depression.R
@@ -1,5 +1,6 @@
 library(aurum)
 library(tidyverse)
+library(survival)
 
 cprdenvname <- ""
 yaml <- ""
@@ -21,17 +22,6 @@ codesets$loadICD10CodeSet(codeSetDf = dep_hosp, name = "icd10_depression", versi
 hosp_dep <- codesets$getAllCodeSetVersion("28/10/25")
 hosp_dep_codes <- hosp_dep$icd10_depression
 
-#New name
-raw_tablename <- "raw_depression_hospitalisation_icd10"
-
-#Get data
-data <- cprd$tables$hesDiagnosisEpi %>%
-  inner_join(hosp_dep_codes, sql_on="LHS.ICD LIKE CONCAT(icd10,'%')") %>%
-  analysis$cached(raw_tablename, indexes=c("patid", "epistart"))
-
-#Assign to new var
-assign(raw_tablename, data)
-
 raw_primary_dep_hospitalisation_icd10 <- raw_depression_hospitalisation_icd10 %>% 
   filter(d_order == 1) %>% analysis$cached("raw_primary_dep_hospitalisation_icd10", indexes = c("patid"))
 
@@ -49,14 +39,47 @@ raw_secondary_dep_hospitalisation_icd10 <- raw_secondary_dep_hospitalisation_icd
   mutate(source="hes_icd10")
 
 #Clean date ranges and add in index dates
-raw_primary_dep_hospitalisation_icd10 <- raw_primary_dep_hospitalisation_icd10 %>%
+clean_primary_dep_hospitalisation_icd10 <- raw_primary_dep_hospitalisation_icd10 %>%
   inner_join(censoring, by="patid") %>%
   filter(date>=min_dob & ((source=="gp" & date<=gp_death_end_date) | ((source=="hes_icd10" | source=="hes_opcs4") & (is.na(gp_death_end_date) | date<=gp_death_end_date)))) %>%
   select(patid, date, source, code)  %>% analysis$cached("clean_primary_dep_hospitalisation")
 
-raw_primary_dep_hospitalisation_icd10 <- raw_primary_dep_hospitalisation_icd10 %>%
+clean_secondary_dep_hospitalisation_icd10 <- raw_secondary_dep_hospitalisation_icd10 %>%
   inner_join(censoring, by="patid") %>%
   filter(date>=min_dob & ((source=="gp" & date<=gp_death_end_date) | ((source=="hes_icd10" | source=="hes_opcs4") & (is.na(gp_death_end_date) | date<=gp_death_end_date)))) %>%
   select(patid, date, source, code) %>% analysis$cached("clean_secondary_dep_hospitalisation")
+
+
+#Next, we get the first date at which someone has a primary or secondary diagnosis 
+#after index, And create an indicator where events prior to index are not allowed.
+analysis <- cprd$analysis("dh_depsev")
+index <- index %>% analysis$cached("AD_informed_index_dates")
+
+#Keep dates for everyone not just AD users
+# If I remove the people where an SSRI isn't the first drug, I lose so many people
+# Is this surprising? probably not... but it's a lot higher in people with a 
+# code for depression
+
+index <- index %>% filter(!is.na(AD_informed_index_date) & AD_index_registration_90days == 1) #I remove ~1/3rd of people but ~1/2 of hospitalisations?
+
+first_primary_hospitalisation <- #Most are after index thank god
+  clean_primary_dep_hospitalisation_icd10 %>%
+  inner_join(index, by = "patid") %>% 
+  mutate(after_index = ifelse(date > AD_informed_index_date, 1, 0)) %>%
+  group_by(patid) %>% 
+  window_order(date) %>%
+  filter(row_number() == 1) %>%
+  ungroup %>% 
+  select(patid, date, code, after_index) %>% analysis$cached("primary_hospitalisation")
+
+
+first_secondary_hospitalisation <- clean_secondary_dep_hospitalisation_icd10 %>%
+  inner_join(index, by = "patid") %>% 
+  mutate(after_index = ifelse(date > AD_informed_index_date, 1, 0)) %>%
+  group_by(patid) %>% 
+  window_order(date) %>%
+  filter(row_number() == 1) %>%
+  ungroup %>% 
+  select(patid, date, code, after_index) %>% analysis$cached("secondary_hospitalisation")
 
 
